a8e29119480b36f601c8a3c8c7b430f83b693956
Luca Ferrandino <luca.ferrandino@windtre.it>
2022-06-27 16:01:49 +0200

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,52 @@
+variable "MD_RUN" { 
+ description = "Virtual Machine Id"
+ type = number
+ default = "0"
+}
+
+
+
+
+variable "MD_VIRTUAL_MACHINE_ID" { 
+ description = "Virtual Machine Id"
+ type = string
+ default = ""
+}
+
+
+
+
+variable "MD_USERNAME" { 
+ description = "UserName for AD Join"
+ type = string
+ default = ""
+ sensitive   = true
+}
+
+variable "MD_USERPWD" { 
+ description = "Password for AD Join"
+ type = string
+ default = ""
+ sensitive   = true
+}
+
+variable "MD_PROJECT_NAME" { 
+ description = "NOme Progetto"
+ type = string
+ default = ""
+ 
+}
+
+variable "MD_DOMAIN_LDAP" { 
+ description = "Percorso LDAP"
+ type = string
+ default = ""
+ 
+}
+
+variable "MD_DOMAIN_NAME" { 
+ description = "Name of the Domain you wish to join"
+ type = string
+ default = ""
+ }
+
---
+++ vm_dom_join.tf
@@ -0,0 +1,32 @@
+
+resource "azurerm_virtual_machine_extension" "domjoin" {
+count = var.MD_RUN
+name = "domjoin"
+virtual_machine_id = var.MD_VIRTUAL_MACHINE_ID
+publisher = "Microsoft.Compute"
+type = "JsonADDomainExtension"
+type_handler_version = "1.3"
+settings = <<SETTINGS
+    {
+    "Name": "${var.MD_DOMAIN_NAME}",
+    "User": "${var.MD_USERNAME}",
+    "OUPath": "OU=${var.MD_PROJECT_NAME},${var.MD_DOMAIN_LDAP}",
+    "Restart": "true",
+    "Options": "3"
+      
+    }
+SETTINGS
+    protected_settings = <<PROTECTED_SETTINGS
+    {
+            "Password": "${var.MD_USERPWD}"
+    }
+PROTECTED_SETTINGS
+lifecycle {
+    ignore_changes = [
+     tags,
+     protected_settings
+     ]
+  }
+}
+
+
